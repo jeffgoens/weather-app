72db7526f0f67b32fff326d707d2fde20099e657
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-26 17:46:34 +0000

    Update

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ let randomAdice = [];
 function getRandomAdvice () {
 fetch('https://api.adviceslip.com/advice')
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data.slip.advice));
 
 }
 
 getRandomAdvice ();
+console.log(data);
+
+function showAdice () {
+    document.getElementById("hello").innerHTML = getRandomAdvice;
+}
