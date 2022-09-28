14c70757a4a2264e90fded47d97bb58a478f07c3
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-28 11:46:11 +0000

    Updates

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-const theButton = document.getElementById('myButton')
+const apiKey = '31b7dec44d4017661c9be2f158f9ae70';
 
-async function getRandomAdvice(url) {
+
+async function getWeatherData() {
     try {
-        const response = await axios.get(url);
-        const fact1 = response.data.slip.advice;
-        document.getElementById("hello").innerHTML = fact1;
-    } catch (error) {
-        console.error(error);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=40509,us&appid=${apikey}`);
+        console.log(response.data.results);
+        const weatherData = response.data.results;
     }
-
 }
 
-getRandomAdvice('https://api.adviceslip.com/advice');
+// const theButton = document.getElementById('myButton')
 
-theButton.addEventListener('click', () => {
-    fact1   
-    contentBlock.innerText = data.slip.advice;
-})
+// async function getRandomAdvice(url) {
+//     try {
+//         const response = await axios.get(url);
+//         const fact1 = response.data.slip.advice;
+//         document.getElementById("hello").innerHTML = fact1;
+//     } catch (error) {
+//         console.error(error);
+//     }
+
+// }
+
+// getRandomAdvice('https://api.adviceslip.com/advice');
+
+// theButton.addEventListener('click', () => {
+//     fact1   
+//     contentBlock.innerText = data.slip.advice;
+// })
 
 
 
