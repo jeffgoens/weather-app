0bb1527669438747fa86777c9eaa4893247e24cf
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-28 17:52:54 +0000

    Update file

--- main.js
+++ main.js
@@ -3,50 +3,53 @@ const apiKey = '31b7dec44d4017661c9be2f158f9ae70';
 
 async function getWeatherData() {
     try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=40509,us&appid=${apikey}`);
-        console.log(response.data.results);
-        const weatherData = response.data.results;
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=40509,us&appid=${apiKey}&units=imperial`);
+        console.log(response.data);
+        // const weatherData = response.data;
+    } catch (error) {
+        console.error(error);
     }
 }
 
-// const theButton = document.getElementById('myButton')
+getWeatherData();
 
-// async function getRandomAdvice(url) {
-//     try {
-//         const response = await axios.get(url);
-//         const fact1 = response.data.slip.advice;
-//         document.getElementById("hello").innerHTML = fact1;
-//     } catch (error) {
-//         console.error(error);
-//     }
+function initPage () {
+    let headerText = document.createElement('h1');
+    headerText.innerText = 'Weather App';
+    document.body.appendChild(headerText);
+    headerText.id = 'headerId';
 
-// }
+    let zipcodeInput = document.createElement('input');
+    zipcodeInput.placeholder = 'Enter 5-digit zipcode';
+    document.body.appendChild(zipcodeInput);
+    zipcodeInput.id = 'zipcodeId';
 
-// getRandomAdvice('https://api.adviceslip.com/advice');
+    let weatherButton = document.createElement('button');
+    weatherButton.innerHTML = "Get Weather";
+    document.body.appendChild(weatherButton);
 
-// theButton.addEventListener('click', () => {
-//     fact1   
-//     contentBlock.innerText = data.slip.advice;
-// })
+}
+
+initPage();
+
+// document.body.onload = addElement;
+// function addElement () {
+//     const newHeader = document.createElement("h1");
+//     const headerContent = document.createTextNode("Weather App");
+//     newHeader.appendChild(headerContent);
+//     const currentHeader = document.getElementById("main");
+//     document.body.insertBefore(newHeader, currentHeader);
+
+//     const newInput = document.createElement('input');
+//     const inputContent = document.createTextNode("zipcode")
+//     newInput.appendChild(inputContent);
+//     const currentInput = document.getElementById("main");
+//     document.body.insertBefore(newInput, currentInput);
+
+    
+    
 
 
 
 
 
-
-
-
-
-// function getRandomAdvice () {
-// fetch('https://api.adviceslip.com/advice')
-//     .then((response) => response.json())
-//     // .then((data) => console.log(data.slip.advice));
-//     .then((data) => {
-//         let fact1 = data.slip.advice;
-//         console.log(fact1);
-//         document.getElementById("hello").innerHTML = fact1
-//     } )
-
-// }
-
-// getRandomAdvice ();
